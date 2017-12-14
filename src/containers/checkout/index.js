4df0fa4dff83bae93fc397f0836853420b339801
Brendon Co <brendon@cxagroup.com>
2017-12-14 16:31:40 +0800

    Cleanup, remove checkout page

--- src/containers/checkout/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <div>
-    <h1>Checkout</h1>
-    <p>Hello Medium!</p>
-  </div>
-);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './redux/store/configureStore';
 import './style/all.scss';
-// import App from './App';
 import App from './containers/app';
 import registerServiceWorker from './registerServiceWorker';
 
